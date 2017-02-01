cdd9b6e992f53622fe07efca51752a160a8f91ba
jayphi <jphinizy@vcu.edu>
2017-02-01 17:46:36 -0500

    file cleanup

--- archive/buildsheetdecoder-pdfquery.py
+++
@@ -1,8 +0,0 @@
-import pdfquery
-
-pdf1 = pdfquery.PDFQuery('bs1.pdf')
-pdf1.load()
-
-#pdf = pdf.tree.tostring()
-
-print(pdf1.tree.write(filename, pretty_print=True))
--- archive/buildsheetdecorder-pypdf2.py
+++
@@ -1,5 +0,0 @@
-from PyPDF2 import PdfFileReader
-pdf_file = 'bs1.pdf'
-read_pdf = PdfFileReader(pdf_file, 'rb')
-page = read_pdf.getPage(1)
-print(page.extractText)
