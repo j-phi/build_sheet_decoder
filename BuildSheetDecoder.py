42f52a264df80563c8534515e8f1bf9363f8d656
jayphi <jphinizy@vcu.edu>
2017-02-02 10:07:31 -0500

    Fix for mfgInstalledList() missing first value.

--- BuildSheetDecoder.py
+++ BuildSheetDecoder.py
@@ -96,7 +96,7 @@ def mfgInstalledList():
     """
     spanList = [s.text for s in soup.find_all('span')]
     spanParents = [s.parent.text for s in soup.find_all('span')]
-    instList = find(spanList, '- inst',0)
+    instList = find(spanList, '- inst', -1) # Start with span before '-inst'
     fnlInstLst = []
     print('\nInstalled Items (MFG):')
     for l in instList[1:]: #([1:] because first inst is in Legend)
